b885e34f5459d9f168094d85ad75d9edf3c421a7
kais3rp <cesare@polonara.com>
2023-04-17 03:54:38 +0200

    fix: missing React import

--- src/Gradient.js
+++ src/Gradient.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Gradient({ name, color, bgColor }) {
   return (
     <svg
--- src/SVGStar.js
+++ src/SVGStar.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function SVGStar({
   color,
   borderColor,
